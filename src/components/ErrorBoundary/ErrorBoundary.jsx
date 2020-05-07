45cddccbfd25a1a8d60b2969f1cf7405cfa5703b
pulgupta <pulgupta@gmail.com>
2020-05-07 12:23:06 +0530

    added more styling

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -1,29 +1,32 @@
 import React, { Component } from "react";
+import "./errorBoundary.css";
 
 class ErrorBoundary extends Component {
+  state = {
+    hasErrors: false,
+    ErrorMessage: "",
+  };
 
-    state = {
-        hasErrors: false,
-        ErrorMessage: ""
-    };
+  // This will catch the error when thrown by the child component
+  // Example throw new Error();
+  componentDidCatch = (error) => {
+    this.setState({
+      hasErrors: true,
+      ErrorMessage: error,
+    });
+  };
 
-    // This will catch the error when thrown by the child component
-    // Example throw new Error();
-    componentDidCatch = (error) => {
-        this.setState({
-            hasErrors: true,
-            ErrorMessage: error
-        })
+  render() {
+    if (this.state.hasErrors) {
+      return (
+        <div>
+          <p className="error">Something went wrong</p>
+        </div>
+      ); 
+    } else {
+      return this.props.children;
     }
-
-    render() {
-        if(this.state.hasErrors) {
-            return <div>Something went wrong</div>
-        } else {
-            return this.props.children
-        }
-    }
-
+  }
 }
 
 export default ErrorBoundary;
