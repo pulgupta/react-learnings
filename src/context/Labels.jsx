cc9fb9f2fd853df125bf52a5f1e8220e96993cd7
pulgupta <pulgupta@gmail.com>
2020-05-08 13:55:13 +0530

    class based static contextType is working now

--- src/context/Labels.jsx
+++ src/context/Labels.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import AppLabels from './AppLabels';
 
+/**
+ * A way to include context using the static property contextType in class based components
+ * We can then access context using this.context
+ * 
+ * Also for functional components we have already demonstrated how to use useContext react hook 
+ */
 class LabelWrapper extends Component {
     
-    static labels = AppLabels;
+    // The value of this variable should always be contextType.
+    static contextType = AppLabels;
     
     componentDidMount() {
         console.log('Labels.jsx')
-        //console.log(this.labels.name)
+        // It should always be named as context
+        console.log(this.context.name)
     }
     
     render() {
-    return <div>a</div>
+    return <></>
     }
 }
 
