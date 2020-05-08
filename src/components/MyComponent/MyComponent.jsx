1986aae177b8b6c70633da4c24e34dea11b11a36
pulgupta <pulgupta@gmail.com>
2020-05-08 08:38:48 +0530

    focusing on some control using reference

--- src/components/MyComponent/MyComponent.jsx
+++ src/components/MyComponent/MyComponent.jsx
@@ -10,10 +10,14 @@ class MyComponent extends Component {
   constructor(props) {
     super(props); // Mandatory is we are using an explicit constructor
     console.log("This is called as the first life cycle method");
+    // creating a reference. We still have to assign it to some control.
+    this.buttonRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log("componentDidMount executed");
+    console.log("componentDidMount executed after the render method");
+    // Since componentDidMount is executed after render() we can now focus on the rendered button.
+    this.buttonRef.current.focus();
   }
 
   // STATE MANAGEMENT: The way to define component state in class based components
@@ -38,13 +42,15 @@ class MyComponent extends Component {
     return (
       <Aux>
         <div>
-          <p>Just to show two divs at same order inside our own React.Fragment</p>
+          <p>
+            Just to show two divs at same order inside our own React.Fragment
+          </p>
         </div>
         <div>
           {
             // way of linling a call without actually calling it
           }
-          <button onClick={this.myEventHandler}>Press Button</button>
+          <button onClick={this.myEventHandler} ref={this.buttonRef}>Press Button</button>
           {this.state.isClicked && (
             // This shows that even after we set the partial state the remaining stuff is preserved and is not removed.
             <p>Button is pressed {this.state.isPatchProof}</p>
@@ -60,6 +66,6 @@ class MyComponent extends Component {
 // This will just generate a warning but will not fail the build or run of the application
 MyComponent.propTypes = {
   name: PropTypes.string,
-}
+};
 
 export default MyComponent;
