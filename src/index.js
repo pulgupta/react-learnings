657cc616dc443decd38b18641a369623b3080254
pulgupta <pulgupta@gmail.com>
2020-05-08 20:52:19 +0530

    added request and response interceptors for our axios calls

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log('This is request inside our interceptor', request);
+    return request;
+}, error => {
+    console.log('This is inside the request error case of our interceptor')
+    return Promise.reject(error);
+});
+
+
+axios.interceptors.response.use(response => {
+    console.log('This is response inside our interceptor', response);
+    return response;
+}, error => {
+    console.log('This is inside the response error case of our interceptor')
+    return Promise.reject(error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
